742c640a6cd413d75ea5277e1a413e17f977ac69
Jeroen Koekkoek <jeroen@koekkoek.nl>
2022-07-06 18:27:07 +0200

    Make b64_pton thread safe and take input length

--- src/base64.c
+++ src/base64.c
@@ -1,4 +1,37 @@
 /*
+ * Copyright (c) 2022, NLnet Labs. All rights reserved.
+ *
+ * This software is open source.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the NLNET LABS nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * Copyright (c) 1996, 1998 by Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -39,8 +72,6 @@
  * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE, EVEN
  * IF IBM IS APPRISED OF THE POSSIBILITY OF SUCH DAMAGES.
  */
-//#include <config.h>
-
 #include <sys/types.h>
 #include <sys/param.h>
 #include <sys/socket.h>
@@ -48,10 +79,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define Assert(Cond) if (!(Cond)) abort()
 
@@ -128,57 +159,59 @@ static const char Pad64 = '=';
    it returns the number of data bytes stored at the target, or -1 on error.
  */
 
-static int b64rmap_initialized = 0;
-static uint8_t b64rmap[256];
+static uint8_t b64rmap[256] = {
+	0xfd, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /*   0 -   7 */
+	0xff, 0xfe, 0xfe, 0xfe,  0xfe, 0xfe, 0xff, 0xff,  /*   8 -  15 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /*  16 -  23 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /*  24 -  31 */
+	0xfe, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /*  32 -  39 */
+	0xff, 0xff, 0xff, 0x3e,  0xff, 0xff, 0xff, 0x3f,  /*  40 -  47 */
+	0x34, 0x35, 0x36, 0x37,  0x38, 0x39, 0x3a, 0x3b,  /*  48 -  55 */
+	0x3c, 0x3d, 0xff, 0xff,  0xff, 0xfd, 0xff, 0xff,  /*  56 -  63 */
+	0xff, 0x00, 0x01, 0x02,  0x03, 0x04, 0x05, 0x06,  /*  64 -  71 */
+	0x07, 0x08, 0x09, 0x0a,  0x0b, 0x0c, 0x0d, 0x0e,  /*  72 -  79 */
+	0x0f, 0x10, 0x11, 0x12,  0x13, 0x14, 0x15, 0x16,  /*  80 -  87 */
+	0x17, 0x18, 0x19, 0xff,  0xff, 0xff, 0xff, 0xff,  /*  88 -  95 */
+	0xff, 0x1a, 0x1b, 0x1c,  0x1d, 0x1e, 0x1f, 0x20,  /*  96 - 103 */
+	0x21, 0x22, 0x23, 0x24,  0x25, 0x26, 0x27, 0x28,  /* 104 - 111 */
+	0x29, 0x2a, 0x2b, 0x2c,  0x2d, 0x2e, 0x2f, 0x30,  /* 112 - 119 */
+	0x31, 0x32, 0x33, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 120 - 127 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 128 - 135 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 136 - 143 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 144 - 151 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 152 - 159 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 160 - 167 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 168 - 175 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 176 - 183 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 184 - 191 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 192 - 199 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 200 - 207 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 208 - 215 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 216 - 223 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 224 - 231 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 232 - 239 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 240 - 247 */
+	0xff, 0xff, 0xff, 0xff,  0xff, 0xff, 0xff, 0xff,  /* 248 - 255 */
+};
 
 static const uint8_t b64rmap_special = 0xf0;
 static const uint8_t b64rmap_end = 0xfd;
 static const uint8_t b64rmap_space = 0xfe;
 static const uint8_t b64rmap_invalid = 0xff;
 
-/**
- * Initializing the reverse map is not thread safe. 
- * Which is fine for NSD. For now...
- **/
-static void
-b64_initialize_rmap ()
-{
-	int i;
-
-	/* Null: end of string, stop parsing */
-	b64rmap[0] = b64rmap_end;
-
-	for (i = 1; i < 256; ++i) {
-		/* Whitespaces */
-		if (isspace(i))
-			b64rmap[i] = b64rmap_space;
-		/* Padding: stop parsing */
-		else if (i == (unsigned char)Pad64)
-			b64rmap[i] = b64rmap_end;
-		/* Non-base64 char */
-		else
-			b64rmap[i] = b64rmap_invalid;
-	}
-
-	/* Fill reverse mapping for base64 chars */
-	for (i = 0; Base64[i] != '\0'; ++i)
-		b64rmap[(uint8_t)Base64[i]] = i;
-
-	b64rmap_initialized = 1;
-}
-
 static int
-b64_pton_do(unsigned char const *src, uint8_t *target, size_t targsize)
+b64_pton_do(uint8_t const *src, size_t len, uint8_t *target, size_t targsize)
 {
 	int tarindex, state, ch;
 	uint8_t ofs;
+  size_t cnt = 0;
 
 	state = 0;
 	tarindex = 0;
 
 	while (1)
 	{
-		ch = *src++;
+		ch = cnt < len ? src[cnt++] : '\0';
 		ofs = b64rmap[ch];
 
 		if (ofs >= b64rmap_special) {
@@ -235,7 +268,7 @@ b64_pton_do(unsigned char const *src, uint8_t *target, size_t targsize)
 	 */
 
 	if (ch == Pad64) {		/* We got a pad char. */
-		ch = *src++;		/* Skip it, get next. */
+		ch = cnt < len ? src[cnt++] : '\0';		/* Skip it, get next. */
 		switch (state) {
 		case 0:		/* Invalid = in first position */
 		case 1:		/* Invalid = in second position */
@@ -243,13 +276,13 @@ b64_pton_do(unsigned char const *src, uint8_t *target, size_t targsize)
 
 		case 2:		/* Valid, means one byte of info */
 			/* Skip any number of spaces. */
-			for ((void)NULL; ch != '\0'; ch = *src++)
+			for ((void)NULL; ch != '\0'; ch = cnt < len ? src[cnt++] : '\0')
 				if (b64rmap[ch] != b64rmap_space)
 					break;
 			/* Make sure there is another trailing = sign. */
 			if (ch != Pad64)
 				return (-1);
-			ch = *src++;		/* Skip the = */
+			ch = cnt < len ? src[cnt++] : '\0';		/* Skip the = */
 			/* Fall through to "single trailing =" case. */
 			/* FALLTHROUGH */
 
@@ -258,7 +291,7 @@ b64_pton_do(unsigned char const *src, uint8_t *target, size_t targsize)
 			 * We know this char is an =.  Is there anything but
 			 * whitespace after it?
 			 */
-			for ((void)NULL; ch != '\0'; ch = *src++)
+			for ((void)NULL; ch != '\0'; ch = cnt < len ? src[cnt++] : '\0')
 				if (b64rmap[ch] != b64rmap_space)
 					return (-1);
 
@@ -285,7 +318,7 @@ b64_pton_do(unsigned char const *src, uint8_t *target, size_t targsize)
 
 
 static int
-b64_pton_len(unsigned char const *src)
+b64_pton_len(uint8_t const *src, size_t len)
 {
 	int tarindex, state, ch;
 	uint8_t ofs;
@@ -295,7 +328,7 @@ b64_pton_len(unsigned char const *src)
 
 	while (1)
 	{
-		ch = *src++;
+		ch = cnt < len ? src[cnt++] : '\0';
 		ofs = b64rmap[ch];
 
 		if (ofs >= b64rmap_special) {
@@ -336,7 +369,7 @@ b64_pton_len(unsigned char const *src)
 	 */
 
 	if (ch == Pad64) {		/* We got a pad char. */
-		ch = *src++;		/* Skip it, get next. */
+		ch = cnt < len ? src[cnt++] : '\0';		/* Skip it, get next. */
 		switch (state) {
 		case 0:		/* Invalid = in first position */
 		case 1:		/* Invalid = in second position */
@@ -344,13 +377,13 @@ b64_pton_len(unsigned char const *src)
 
 		case 2:		/* Valid, means one byte of info */
 			/* Skip any number of spaces. */
-			for ((void)NULL; ch != '\0'; ch = *src++)
+			for ((void)NULL; ch != '\0'; ch = cnt < len ? src[cnt++] : '\0')
 				if (b64rmap[ch] != b64rmap_space)
 					break;
 			/* Make sure there is another trailing = sign. */
 			if (ch != Pad64)
 				return (-1);
-			ch = *src++;		/* Skip the = */
+			ch = cnt < len ? src[cnt++] : '\0';		/* Skip the = */
 			/* Fall through to "single trailing =" case. */
 			/* FALLTHROUGH */
 
@@ -359,7 +392,7 @@ b64_pton_len(unsigned char const *src)
 			 * We know this char is an =.  Is there anything but
 			 * whitespace after it?
 			 */
-			for ((void)NULL; ch != '\0'; ch = *src++)
+			for ((void)NULL; ch != '\0'; ch = cnt < len ? src[cnt++] : '\0')
 				if (b64rmap[ch] != b64rmap_space)
 					return (-1);
 
@@ -378,13 +411,10 @@ b64_pton_len(unsigned char const *src)
 
 
 int
-b64_pton(char const *src, uint8_t *target, size_t targsize)
+b64_pton(char const *src, size_t len, uint8_t *target, size_t targsize)
 {
-	if (!b64rmap_initialized)
-		b64_initialize_rmap ();
-
 	if (target)
-		return b64_pton_do ((unsigned char const*)src, target, targsize);
+		return b64_pton_do ((uint8_t const*)src, len, target, targsize);
 	else
-		return b64_pton_len ((unsigned char const*)src);
+		return b64_pton_len ((uint8_t const*)src, len);
 }
