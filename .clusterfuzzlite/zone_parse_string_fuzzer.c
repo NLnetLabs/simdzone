0092e2eadbfae068fb500e6a4cc24af2634d12ad
David Korczynski <david@adalogics.com>
2023-12-21 15:41:44 -0800

    Add ClusterFuzzLite integration
    Signed-off-by: David Korczynski <david@adalogics.com>

---
+++ .clusterfuzzlite/zone_parse_string_fuzzer.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "zone.h"
+
+static int32_t add_rr(zone_parser_t *parser, const zone_name_t *owner,
+                      uint16_t type, uint16_t class, uint32_t ttl,
+                      uint16_t rdlength, const uint8_t *rdata,
+                      void *user_data) {
+  (void)parser;
+  (void)owner;
+  (void)type;
+  (void)class;
+  (void)ttl;
+  (void)rdlength;
+  (void)rdata;
+  (void)user_data;
+  return ZONE_SUCCESS;
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  // Ensure we have a bit of data.
+  if (size < 10) {
+    return 0;
+  }
+  zone_parser_t parser = {0};
+  zone_name_buffer_t name;
+  zone_rdata_buffer_t rdata;
+  zone_buffers_t buffers = {1, &name, &rdata};
+  zone_options_t options = {0};
+
+  options.accept.callback = add_rr;
+  options.origin = "example.com.";
+  options.default_ttl = 3600;
+  options.default_class = 1;
+
+  zone_parse_string(&parser, &options, &buffers, (const char *)data, size,
+                    NULL);
+
+  return 0;
+}
