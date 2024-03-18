986f6b39002710a7bc45cf281bb342603a03ab4b
Jeroen Koekkoek <jeroen@koekkoek.nl>
2024-03-18 16:40:27 +0100

    Overhaul file and buffer operation
    * Add buffer limit to avoid hogging all memory on malicious inputs.
    * Drop use of PathIsRelative to remove dependency on shlwapi.
    * Always allocate size+padding.
    * Log an error if $INCLUDE is unsuccessful.

--- src/zone.c
+++ src/zone.c
@@ -14,62 +14,22 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <limits.h>
-#if _WIN32
-# include <windows.h>
-# include <shlwapi.h>
-#endif
+#include <stddef.h>
 
 #include "zone.h"
 
 typedef zone_parser_t parser_t; // convenience
+typedef zone_file_t file_t;
 
 #include "attributes.h"
 #include "diagnostic.h"
-#include "isadetection.h"
 
-#if _WIN32
-# if _MSC_VER
-#   define strcasecmp(s1, s2) _stricmp(s1, s2)
-#   define strncasecmp(s1, s2, n) _strnicmp(s1, s2, n)
-# endif
-
-static char *strndup(const char *s, size_t n)
-{
-  char *p;
-  if ((p = malloc(n + 1))) {
-    memcpy(p, s, n);
-    p[n] = '\0';
-  }
-  return p;
-}
+#if _MSC_VER
+# define strcasecmp(s1, s2) _stricmp(s1, s2)
+# define strncasecmp(s1, s2, n) _strnicmp(s1, s2, n)
 #endif
 
 static const char not_a_file[] = "<string>";
-
-static int32_t check_options(const zone_options_t *options)
-{
-  if (!options->accept.callback)
-    return ZONE_BAD_PARAMETER;
-  if (!options->default_ttl || options->default_ttl > INT32_MAX)
-    return ZONE_BAD_PARAMETER;
-  if (!options->origin.octets || !options->origin.length)
-    return ZONE_BAD_PARAMETER;
-
-  const uint8_t *root = &options->origin.octets[options->origin.length - 1];
-  if (root[0] != 0)
-    return ZONE_BAD_PARAMETER;
-  const uint8_t *label = &options->origin.octets[0];
-  while (label < root) {
-    if (root - label < label[0])
-      return ZONE_BAD_PARAMETER;
-    label += label[0] + 1;
-  }
-
-  if (label != root)
-    return ZONE_BAD_PARAMETER;
-
-  return 0;
-}
 
 #include "config.h"
 #include "isadetection.h"
@@ -142,126 +102,114 @@ static int32_t parse(parser_t *parser, void *user_data)
 diagnostic_push()
 msvc_diagnostic_ignored(4996)
 
-nonnull_all
-static int32_t open_file(
-  parser_t *parser, zone_file_t *file, const char *path, size_t length)
-{
-  char *abs = NULL;
-
-  (void)parser;
-
-  if (!(file->name = strndup(path, length)))
-    return ZONE_OUT_OF_MEMORY;
-
-  const char *rel = file->name;
-
 #if _WIN32
-  char buf[4];
-  // relative include paths are relative to including file
-  if (file != &parser->first && PathIsRelative(path)) {
-    assert(parser->file->path != not_a_file);
-    assert(parser->file->handle != NULL);
-    const char *dir = parser->file->path;
-    int dirlen = 0;
-    for (int i = 0; i < INT32_MAX && dir[i]; i++) {
-      if (dir[i] == '/' || dir[i] == '\\')
-        dirlen = i + 1;
-    }
-    int len;
-    len = snprintf(buf, sizeof(buf), "%.*s\\%s", dirlen, dir, file->name);
-    assert(len != -1);
-    if (!(abs = malloc(len + 1)))
-      return ZONE_READ_ERROR;
-    (void)snprintf(abs, len + 1, "%.*s\\%s", dirlen, dir, file->name);
-    rel = abs;
-  }
-  DWORD size = GetFullPathName(rel, sizeof(buf), buf, NULL);
-  if (!size)
-    goto read_error;
-  if (!(file->path = malloc(size)))
-    goto out_of_memory;
-  (void)GetFullPathName(rel, size, file->path, NULL);
-#else
-  char buf[PATH_MAX];
-  if (file != &parser->first && path[0] != '/') {
-    assert(parser->file->path != not_a_file);
-    assert(parser->file->handle != NULL);
-    const char *dir = parser->file->path;
-    int dirlen = 0;
-    for (int i = 0; i < INT32_MAX && dir[i]; i++) {
-      if (dir[i] == '/')
-        dirlen = i + 1;
-    }
-    int len;
-    len = snprintf(buf, sizeof(buf), "%.*s/%s", dirlen, dir, file->name);
-    if (!(abs = malloc((size_t)len + 1)))
-      return ZONE_OUT_OF_MEMORY;
-    (void)snprintf(abs, (size_t)len + 1, "%.*s/%s", dirlen, dir, file->name);
-    rel = abs;
-  }
-  if (!realpath(rel, buf))
-    goto read_error;
-  if (!(file->path = strdup(buf)))
-    goto out_of_memory;
-#endif
-  if (abs)
-    free(abs);
-  abs = NULL;
-
-  if (!(file->handle = fopen(file->path, "rb")))
-    switch (errno) {
-      case ENOMEM:
-        return ZONE_OUT_OF_MEMORY;
-      default:
-        return ZONE_READ_ERROR;
-    }
-
-  if (!(file->buffer.data = malloc(ZONE_WINDOW_SIZE + 1)))
-    return ZONE_OUT_OF_MEMORY;
-
-  file->buffer.data[0] = '\0';
-  file->buffer.size = ZONE_WINDOW_SIZE;
-  file->buffer.length = 0;
-  file->buffer.index = 0;
-  file->start_of_line = true;
-  file->end_of_file = 0;
-  file->fields.tape[0] = file->fields.tape[1] = file->buffer.data;
-  file->fields.head = file->fields.tape;
-  file->fields.tail = file->fields.tape;
-  file->lines.tape[0] = 0;
-  file->lines.head = file->lines.tape;
-  file->lines.tail = file->lines.tape;
-  return 0;
-read_error:
-  if (abs) free(abs);
-  return ZONE_READ_ERROR;
-out_of_memory:
-  if (abs) free(abs);
-  return ZONE_OUT_OF_MEMORY;
-}
-
-diagnostic_pop()
-
-static void set_defaults(parser_t *parser)
+nonnull_all
+static bool is_separator(int c)
 {
-  if (!parser->options.log.callback && !parser->options.log.categories)
-    parser->options.log.categories = (uint32_t)-1;
-  parser->owner = &parser->file->owner;
-  parser->rdata = &parser->buffers.rdata.blocks[0];
+  return c == '\\' || c == '/';
 }
-
-diagnostic_push()
-clang_diagnostic_ignored(missing-prototypes)
 
 nonnull_all
-void zone_close_file(
-  parser_t *parser, zone_file_t *file)
+static bool is_rooted(const char *s)
 {
-  assert(file);
+  if ((s[0] >= 'A' && s[0] <= 'Z') || (s[0] >= 'a' && s[0] <= 'z'))
+    return s[1] == ':';
+  return false;
+}
+
+nonnull_all
+static bool is_relative(const char *s)
+{
+  // rooted paths can be relative, e.g. C:foo
+  if (is_rooted(s))
+    return !is_separator((unsigned char)s[2]);
+  if (is_separator((unsigned char)s[0]))
+    return !(s[1] == '?' || is_separator((unsigned char)s[1]));
+  return false;
+}
+
+// The Win32 API offers PathIsRelative, but it requires linking with shlwapi.
+// Rewriting a relative path is not too complex, unlike correct conversion of
+// Windows paths in general (https://googleprojectzero.blogspot.com/2016/02/).
+// Rooted paths, relative or not, unc and extended paths are never resolved
+// relative to the includer.
+nonnull((2,3))
+static int32_t resolve_path(
+  const char *includer, const char *include, char **path)
+{
+  // support relative non-rooted paths only
+  if (*includer && is_relative(include) && !is_rooted(include)) {
+    assert(!is_relative(includer));
+    const char *separator = include;
+    for (const char *p = include; *p; p++)
+      if (is_separator((unsigned char)*p))
+        separator = p;
+    if (separator - include > INT_MAX)
+      return ZONE_OUT_OF_MEMORY;
+    char buffer[16];
+    int offset = (int)(separator - includer);
+    int length = snprintf(
+      buffer, sizeof(buffer), "%.*s/%s", offset, includer, include);
+    if (length < 0)
+      return ZONE_OUT_OF_MEMORY;
+    char *absolute;
+    if (!(absolute = malloc(length + 1)))
+      return ZONE_OUT_OF_MEMORY;
+    (void)snprintf(
+      absolute, (size_t)length + 1, "%.*s/%s", offset, includer, include);
+    *path = _fullpath(NULL, absolute, 0);
+    free(absolute);
+  } else {
+    *path = _fullpath(NULL, include, 0);
+  }
+
+  if (*path)
+    return 0;
+  return (errno == ENOMEM) ? ZONE_OUT_OF_MEMORY : ZONE_NOT_A_FILE;
+}
+#else
+nonnull_all
+static int32_t resolve_path(
+  const char *includer, const char *include, char **path)
+{
+  if (*includer && *include != '/') {
+    assert(*includer == '/');
+    const char *separator = strrchr(includer, '/');
+    if (separator - include > INT_MAX)
+      return ZONE_OUT_OF_MEMORY;
+    char buffer[16];
+    int offset = (int)(separator - includer);
+    int length = snprintf(
+      buffer, sizeof(buffer), "%.*s/%s", offset, includer, include);
+    if (length < 0)
+      return ZONE_OUT_OF_MEMORY;
+    char *absolute;
+    if (!(absolute = malloc((size_t)length + 1)))
+      return ZONE_OUT_OF_MEMORY;
+    (void)snprintf(
+      absolute, (size_t)length + 1, "%.*s/%s", offset, includer, include);
+    *path = realpath(absolute, NULL);
+    free(absolute);
+  } else {
+    *path = realpath(include, NULL);
+  }
+
+  if (*path)
+    return 0;
+  return (errno == ENOMEM) ? ZONE_OUT_OF_MEMORY : ZONE_NOT_A_FILE;
+}
+#endif
+
+nonnull((1))
+static void close_file(
+  parser_t *parser, file_t *file)
+{
+  assert((file->name == not_a_file) == (file->path == not_a_file));
+
   const bool is_string = file->name == not_a_file || file->path == not_a_file;
 
-  (void)parser;
-  assert(!is_string || !file->handle);
+  assert(!is_string || file == &parser->first);
+  assert(!is_string || file->handle == NULL);
 
   if (file->buffer.data && !is_string)
     free(file->buffer.data);
@@ -275,40 +223,176 @@ void zone_close_file(
   if (file->handle)
     (void)fclose(file->handle);
   file->handle = NULL;
+}
+
+nonnull_all
+static void initialize_file(
+  parser_t *parser, file_t *file)
+{
+  const size_t size = offsetof(file_t, fields.head);
+  memset(file, 0, size);
+
+  if (file == &parser->first) {
+    file->includer = NULL;
+    memcpy(file->origin.octets,
+           parser->options.origin.octets,
+           parser->options.origin.length);
+    file->origin.length = parser->options.origin.length;
+    file->last_class = parser->options.default_class;
+    file->last_ttl = parser->options.default_ttl;
+  } else {
+    assert(parser->file);
+    file->includer = parser->file;
+    memcpy(&file->origin, &parser->file->origin, sizeof(file->origin));
+    // retain class and TTL
+    file->last_class = parser->file->last_class;
+    file->last_ttl = parser->file->last_ttl;
+  }
+
+  file->line = 1;
+  file->name = (char *)not_a_file;
+  file->path = (char *)not_a_file;
+  file->handle = NULL;
+  file->buffer.data = NULL;
+  file->start_of_line = true;
+  file->end_of_file = 1;
+  file->fields.tape[0] = NULL;
+  file->fields.head = file->fields.tail = file->fields.tape;
+  file->delimiters.tape[0] = NULL;
+  file->delimiters.head = file->delimiters.tail = file->delimiters.tape;
+  file->lines.tape[0] = 0;
+  file->lines.head = file->lines.tail = file->lines.tape;
+}
+
+nonnull_all
+static int32_t open_file(
+  parser_t *parser, file_t *file, const char *include, size_t length)
+{
+  int32_t code;
+  const size_t size = ZONE_WINDOW_SIZE + 1 + ZONE_PADDING_SIZE;
+
+  initialize_file(parser, file);
+
+  if (!(file->name = malloc(length + 1)))
+    return ZONE_OUT_OF_MEMORY;
+  memcpy(file->name, include, length);
+  file->name[length] = '\0';
+  if (!(file->buffer.data = malloc(size)))
+    return (void)close_file(parser, file), ZONE_OUT_OF_MEMORY;
+  file->buffer.data[0] = '\0';
+  file->buffer.size = ZONE_WINDOW_SIZE;
+  file->end_of_file = 0;
+  file->fields.tape[0] = &file->buffer.data[0];
+  file->fields.tape[1] = &file->buffer.data[0];
+
+  const char *includer = "";
   if (file != &parser->first)
-    free(file);
+    includer = parser->file->path;
+  if ((code = resolve_path(includer, file->name, &file->path)))
+    return (void)close_file(parser, file), code;
+
+  if ((file->handle = fopen(file->path, "rb")))
+    return 0;
+
+  switch (errno) {
+    case ENOMEM:
+      code = ZONE_OUT_OF_MEMORY;
+      break;
+    case EACCES:
+      code = ZONE_NOT_PERMITTED;
+      break;
+    default:
+      code = ZONE_NOT_A_FILE;
+      break;
+  }
+
+  close_file(parser, file);
+  return code;
+}
+
+diagnostic_pop()
+
+diagnostic_push()
+clang_diagnostic_ignored(missing-prototypes)
+
+nonnull((1))
+void zone_close_file(
+  parser_t *parser, zone_file_t *file)
+{
+  if (!file)
+    return;
+  close_file(parser, file);
+  free(file);
 }
 
 nonnull_all
 int32_t zone_open_file(
-  parser_t *parser, const char *path, size_t length, zone_file_t **fileptr)
+  parser_t *parser, const char *path, size_t length, zone_file_t **file)
 {
-  zone_file_t *file;
-  int32_t result;
+  int32_t code;
 
-  if (!(file = malloc(sizeof(*file))))
+  if (!(*file = malloc(sizeof(**file))))
     return ZONE_OUT_OF_MEMORY;
-  memset(file, 0, sizeof(*file));
-  if ((result = open_file(parser, file, path, length)) < 0)
-    goto err_open;
-
-  *fileptr = file;
+  if ((code = open_file(parser, *file, path, length)) < 0)
+    return (void)free(*file), code;
   return 0;
-err_open:
-  zone_close_file(parser, file);
-  return result;
 }
 
+nonnull_all
 void zone_close(parser_t *parser)
 {
-  if (!parser)
-    return;
-
+  assert(parser);
   for (zone_file_t *file = parser->file, *includer; file; file = includer) {
     includer = file->includer;
-    if (file->handle)
-      zone_close_file(parser, file);
+    close_file(parser, file);
+    if (file != &parser->first)
+      free(file);
   }
+}
+
+nonnull((1,2,3))
+static int32_t initialize_parser(
+  zone_parser_t *parser,
+  const zone_options_t *options,
+  zone_buffers_t *buffers,
+  void *user_data)
+{
+  if (!options->accept.callback)
+    return ZONE_BAD_PARAMETER;
+  if (!options->default_ttl)
+    return ZONE_BAD_PARAMETER;
+  if (!options->non_strict && options->default_ttl > INT32_MAX)
+    return ZONE_BAD_PARAMETER;
+  if (!options->origin.octets || !options->origin.length)
+    return ZONE_BAD_PARAMETER;
+
+  const uint8_t *root = &options->origin.octets[options->origin.length - 1];
+  if (root[0] != 0)
+    return ZONE_BAD_PARAMETER;
+  const uint8_t *label = &options->origin.octets[0];
+  while (label < root) {
+    if (root - label < label[0])
+      return ZONE_BAD_PARAMETER;
+    label += label[0] + 1;
+  }
+
+  if (label != root)
+    return ZONE_BAD_PARAMETER;
+
+  const size_t size = offsetof(parser_t, file);
+  memset(parser, 0, size);
+  parser->options = *options;
+  parser->user_data = user_data;
+  parser->file = &parser->first;
+  parser->buffers.size = buffers->size;
+  parser->buffers.owner.active = 0;
+  parser->buffers.owner.blocks = buffers->owner;
+  parser->buffers.rdata.active = 0;
+  parser->buffers.rdata.blocks = buffers->rdata;
+  parser->owner = &parser->buffers.owner.blocks[0];
+  parser->owner->length = 0;
+  parser->rdata = &parser->buffers.rdata.blocks[0];
+  return 0;
 }
 
 int32_t zone_open(
@@ -318,35 +402,13 @@ int32_t zone_open(
   const char *path,
   void *user_data)
 {
-  zone_file_t *file;
-  int32_t result;
+  int32_t code;
 
-  if ((result = check_options(options)) < 0)
-    return result;
-
-  memset(parser, 0, sizeof(*parser));
-  parser->options = *options;
-  parser->user_data = user_data;
-  file = parser->file = &parser->first;
-  if ((result = open_file(parser, file, path, strlen(path))) < 0)
-    goto error;
-  memcpy(file->origin.octets, options->origin.octets, options->origin.length);
-  file->origin.length = options->origin.length;
-  parser->buffers.size = buffers->size;
-  parser->buffers.owner.serial = 0;
-  parser->buffers.owner.blocks = buffers->owner;
-  parser->buffers.rdata.blocks = buffers->rdata;
-  file->owner = file->origin;
-  file->last_type = 0;
-  file->last_class = options->default_class;
-  file->last_ttl = options->default_ttl;
-  file->line = 1;
-
-  set_defaults(parser);
+  if ((code = initialize_parser(parser, options, buffers, user_data)) < 0)
+    return code;
+  if ((code = open_file(parser, &parser->first, path, strlen(path))) < 0)
+    return code;
   return 0;
-error:
-  zone_close(parser);
-  return result;
 }
 
 diagnostic_pop()
@@ -358,13 +420,13 @@ int32_t zone_parse(
   const char *path,
   void *user_data)
 {
-  int32_t result;
+  int32_t code;
 
-  if ((result = zone_open(parser, options, buffers, path, user_data)) < 0)
-    return result;
-  result = parse(parser, user_data);
+  if ((code = zone_open(parser, options, buffers, path, user_data)) < 0)
+    return code;
+  code = parse(parser, user_data);
   zone_close(parser);
-  return result;
+  return code;
 }
 
 int32_t zone_parse_string(
@@ -375,50 +437,23 @@ int32_t zone_parse_string(
   size_t length,
   void *user_data)
 {
-  zone_file_t *file;
-  int32_t result;
+  int32_t code;
 
+  if ((code = initialize_parser(parser, options, buffers, user_data)) < 0)
+    return code;
   if (!length || string[length] != '\0')
     return ZONE_BAD_PARAMETER;
-  if ((result = check_options(options)) < 0)
-    return result;
+  initialize_file(parser, parser->file);
+  parser->file->buffer.data = (char *)string;
+  parser->file->buffer.size = length;
+  parser->file->buffer.length = length;
+  parser->file->fields.tape[0] = &string[length];
+  parser->file->fields.tape[1] = &string[length];
+  assert(parser->file->end_of_file == 1);
 
-  memset(parser, 0, sizeof(*parser));
-  parser->options = *options;
-  parser->user_data = user_data;
-  file = parser->file = &parser->first;
-  memcpy(file->origin.octets, options->origin.octets, options->origin.length);
-  file->origin.length = options->origin.length;
-  file->name = (char *)not_a_file;
-  file->path = (char *)not_a_file;
-  file->handle = NULL;
-  file->buffer.index = 0;
-  file->buffer.length = length;
-  file->buffer.size = length;
-  file->buffer.data = (char *)string;
-  file->start_of_line = true;
-  file->end_of_file = 1;
-  file->fields.tape[0] = file->fields.tape[1] = &string[length];
-  file->fields.head = file->fields.tape;
-  file->fields.tail = file->fields.tape;
-  file->lines.tape[0] = 0;
-  file->lines.head = file->lines.tape;
-  file->lines.tail = file->lines.tape;
-
-  parser->buffers.size = buffers->size;
-  parser->buffers.owner.serial = 0;
-  parser->buffers.owner.blocks = buffers->owner;
-  parser->buffers.rdata.blocks = buffers->rdata;
-  file->owner = file->origin;
-  file->last_type = 0;
-  file->last_class = options->default_class;
-  file->last_ttl = options->default_ttl;
-  file->line = 1;
-
-  set_defaults(parser);
-  result = parse(parser, user_data);
+  code = parse(parser, user_data);
   zone_close(parser);
-  return result;
+  return code;
 }
 
 zone_nonnull((1,3))
