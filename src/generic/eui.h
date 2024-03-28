716500731e17d06bb1a48a596915b0a5bd455839
Jeroen Koekkoek <jeroen@koekkoek.nl>
2024-03-28 11:02:00 +0100

    Fix bugs in EUI48 and EUI64 parsers

--- src/generic/eui.h
+++ src/generic/eui.h
@@ -10,21 +10,21 @@
 #define EUI_H
 
 nonnull((1,2))
-static really_inline int
-eui_base16_dec_loop_generic_32_inner(const uint8_t *s, uint8_t *o, int dash)
+static really_inline bool
+eui_base16_dec_loop_generic_32_inner(const uint8_t *s, uint8_t *o, bool last)
 {
   const uint32_t val1 = base16_table_dec_32bit_d0[s[0]]
                       | base16_table_dec_32bit_d1[s[1]];
   const uint32_t val2 = base16_table_dec_32bit_d0[s[3]]
                       | base16_table_dec_32bit_d1[s[4]];
 
-  if (val1 > 0xff || val2 > 0xff || (dash ^ ((s[2] == '-') ^ (s[5] == '-'))))
-    return 0;
+  if (val1 > 0xff || val2 > 0xff || s[2] != '-' || (!last && s[5] != '-'))
+    return false;
 
   o[0] = (uint8_t)val1;
   o[1] = (uint8_t)val2;
 
-  return 1;
+  return true;
 }
 
 // RFC7043 section 3.2, require xx-xx-xx-xx-xx-xx
@@ -38,9 +38,9 @@ static really_inline int32_t parse_eui48(
 {
   const uint8_t *input = (const uint8_t *)token->data;
   if (token->length == 17 &&
-      eui_base16_dec_loop_generic_32_inner(input, rdata->octets, 0) &&
-      eui_base16_dec_loop_generic_32_inner(input+6, rdata->octets+2, 0) &&
-      eui_base16_dec_loop_generic_32_inner(input+12, rdata->octets+4, 1))
+      eui_base16_dec_loop_generic_32_inner(input, rdata->octets, false) &&
+      eui_base16_dec_loop_generic_32_inner(input+6, rdata->octets+2, false) &&
+      eui_base16_dec_loop_generic_32_inner(input+12, rdata->octets+4, true))
     return (void)(rdata->octets += 6), 0;
   SYNTAX_ERROR(parser, "Invalid %s in %s", NAME(field), NAME(type));
 }
@@ -56,10 +56,10 @@ static really_inline int32_t parse_eui64(
 {
   const uint8_t *input = (const uint8_t *)token->data;
   if (token->length == 23 &&
-      eui_base16_dec_loop_generic_32_inner(input, rdata->octets, 0) &&
-      eui_base16_dec_loop_generic_32_inner(input+6, rdata->octets+2, 0) &&
-      eui_base16_dec_loop_generic_32_inner(input+12, rdata->octets+4, 0) &&
-      eui_base16_dec_loop_generic_32_inner(input+18, rdata->octets+6, 1))
+      eui_base16_dec_loop_generic_32_inner(input, rdata->octets, false) &&
+      eui_base16_dec_loop_generic_32_inner(input+6, rdata->octets+2, false) &&
+      eui_base16_dec_loop_generic_32_inner(input+12, rdata->octets+4, false) &&
+      eui_base16_dec_loop_generic_32_inner(input+18, rdata->octets+6, true))
     return (void)(rdata->octets += 8), 0;
   SYNTAX_ERROR(parser, "Invalid %s in %s", NAME(field), NAME(type));
 }
